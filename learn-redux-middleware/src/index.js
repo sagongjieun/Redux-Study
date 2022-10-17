1935ac0931eb2a0063dfdff91d2be27b4e0f0f0c
사공지은 <gonii0821@naver.com>
2022-10-18 00:01:55 +0900

    practice redux thunk

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
+import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
